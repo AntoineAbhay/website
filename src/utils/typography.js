813cf8c6334ff2d2fdd37f801e4c4de4d2261490
Antoine Abhay <antoineabhay@gmail.com>
2020-05-18 23:48:00 +0200

    Change typography theme

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,7 @@
 import Typography from "typography"
-import stowLakeTheme from "typography-theme-stow-lake"
+import sutroTheme from "typography-theme-sutro"
 
-const typography = new Typography(stowLakeTheme)
+const typography = new Typography(sutroTheme)
 
 // Export helper functions
 export const { scale, rhythm, options } = typography
