3abdf25b0d8461a4b87b39a884c96ef5b83b7216
Antoine Abhay <antoineabhay@gmail.com>
2020-12-27 21:03:24 +0100

    debug netlify deploy 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 const activeEnv = process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
 
 console.log(`Using environment config: '${activeEnv}'`)
+console.log(process.env.GITHUB_TOKEN)
 
 require("dotenv").config({
   path: `.env.${activeEnv}`,
